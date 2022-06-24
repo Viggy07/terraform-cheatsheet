d6be6901eb502b8efcd504bc99657d64dd1b15b8
Vignesh <viggy07.us@gmail.com>
2022-06-23 23:19:32 -0400

    Update locals.tf

--- examples/locals.tf
+++ examples/locals.tf
@@ -2,3 +2,16 @@ locals {
   subnet_ids = concat(tolist(data.aws_subnet_ids.public.ids), tolist(data.aws_subnet_ids.private.ids))
 }
 
+locals {
+  envs = {
+    "staging-01" = {
+      tenant_name        = "web-01"
+      env                = "staging-01"
+    },
+
+    "staging-02" = {
+      env                = "staging-02"
+      webserver_vpc_id   = var.webserver_vpc_id
+    }
+  }
+}
