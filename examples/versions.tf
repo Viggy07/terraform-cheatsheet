299123362f0a953ace3a328f3d7f627bb35bbb8f
Vignesh <viggy07.us@gmail.com>
2022-06-23 22:34:09 -0400

    Create versions.tf

---
+++ examples/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws        = ">= 4.0.0"
+    null       = ">= 3.0.0"
+    tls        = ">= 3.0.0"
+    random     = ">= 3.1"
+  }
+}
